193c5299201b3a3efacbca1ec1ca8e3f01d73e4a
Andrew Campbell <andrewcambell@Andrews-iMac.local>
2022-01-05 15:32:32 -0800

    Now rolling two dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,17 @@
+# What packages are being used
+import random
+
+# Main body of the game
 def main():
-  print('You rolled a die')
+  # Variables for amount of dice to roll and total of dice rolled
+  dice_rolls = 2
+  dice_sum = 0
+  # For loop to randomize the dice rolls and calcualte totals
+  for i in range(0,dice_rolls):
+    roll = random.randint(1,6)
+    dice_sum += roll
+    print(f'You rolled a {roll}')
+  print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
   main()
